803808bfb4857ebec4e063acc5e44ae74ea24a80
Bhagwat-45 <cbh.mavim@gmail.com>
2026-07-09 19:21:15 +0530

    Add Book model with fields and database initialization

--- src/db/main.py
+++ src/db/main.py
@@ -1,11 +1,14 @@
-from sqlmodel import create_engine, text
+from sqlmodel import text, SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine
 from src.config import Config
+from src.books.models import Book
 
 engine = create_async_engine(url=Config.DATABASE_URL, echo=True)
 
 async def init_db():
     async with engine.begin() as conn:
-        result = await conn.execute(text("SELECT current_database(), version();"))
-        print(result.all())
+        await conn.run_sync(
+            SQLModel.metadata.create_all
+        )
+        
 
